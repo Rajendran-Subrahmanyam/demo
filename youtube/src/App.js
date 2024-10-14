b4c3071afa84222d0081dd0d113c8ece586f53fe
demo <your@gmail.com>
2024-10-14 12:09:37 +0530

    14oct24

---
+++ youtube/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+
+function App() {
+  return (
+   <>
+    <h1 className='text-primary'>Register
+    </h1><hr/>
+    <span>Enter Username- </span><input type='text'</input> <br/> </br/>
+    {/* <span>Enter Password- </span><input type='text'</input>  <br/> </br/> */}
+<button className='btn btn-success'>Login</button>
+   </>
+  )
+}
+
+export default App;
